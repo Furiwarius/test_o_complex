c998bfd0d81cd06dc2413f42f562e9a8fd92e6ba
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-21 21:55:14 +0300

    fix: removed unnecessary constants from the settings file

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -3,9 +3,4 @@ import os
 
 dotenv.load_dotenv()
 
-DATABASE_USER = os.getenv("DATABASE_USER")
 NAME_DATABASE = os.getenv("NAME_DATABASE")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-HOST = os.getenv("HOST")
-DATABASE_LOG_SETTINGS = os.getenv("DATABASE_LOG_SETTINGS")
-DEVELOPER_EMAIL = os.getenv("DEVELOPER_EMAIL")
