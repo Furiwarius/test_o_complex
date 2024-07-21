fa8e4ad989fff497ec23e3124dbf9f1f9c022d83
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-21 13:21:37 +0300

    fix: database classes fixed; feat: added crud class

--- app/database/cruds.py
+++ app/database/cruds.py
@@ -1,3 +1,30 @@
 from app.database.database import Database
 from datetime import datetime, timezone
+from app.database.tables.essence import HistoryTable
+from sqlalchemy.orm import Session
+
+
+class ApplicationCRUD():
+    '''
+    Класс для добавления пользователей и записей в БД
+    '''
+    
+    def add_record(self, request:str) -> None:
+        '''
+        Добавить запись в таблицу с историей поиска
+        '''
+        with Database() as db:
+            history_id = db.query(HistoryTable.id).filter(HistoryTable.sity==request).all()
+            if len(history_id)==0 :
+                new_history = HistoryTable(sity=request, count=1)
+                db.add(new_history)
+                db.commit()
+                
+            else:
+                history = db.get(HistoryTable, history_id[0])
+                db.query(HistoryTable).filter(HistoryTable.id == history.id).update({HistoryTable.count:history.count+1}, 
+                                                        synchronize_session = False)
+                db.commit()
+                
+            
 
--- app/database/tables/essence.py
+++ app/database/tables/essence.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, Integer, ForeignKey
-from sqlalchemy import Column, DateTime, Boolean
+from sqlalchemy import Column, DateTime, Boolean, func
 from datetime import datetime
 from sqlalchemy.orm import DeclarativeBase
 
@@ -9,20 +9,6 @@ class Base(DeclarativeBase):
     Базовый класс для моделей
     '''
 
-
-class UserTable(Base):
-    '''
-    Модель таблицы user
-    '''
-    __tablename__ = "user"
-
-    # токен по которому будуд отслеживаться пользователи
-    name = Column(String(60), primary_key=True, unique=True)
-    status = Column(Boolean, default=True, nullable=False)
-    create_date = Column(DateTime, default=datetime.now, nullable=False)
-
-
-
 class HistoryTable(Base):
     '''
     Модель таблицы history
@@ -31,7 +17,6 @@ class HistoryTable(Base):
 
     id = Column(Integer, primary_key=True, 
                 index=True, autoincrement='auto', unique=True)
-    user_id = Column(Integer, ForeignKey("user.name"), nullable=False)
     sity = Column(String(60), nullable=False)
-    create_date = Column(DateTime, default=datetime.now, nullable=False)
+    count = Column(Integer, nullable=False)
 
